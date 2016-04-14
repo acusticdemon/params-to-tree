ea1b98d2808d4329ab17f0c2c2d1c56450358a6a
gafarovrenat <r.gafarov@semrush.com>
2016-04-14 17:30:54 +0300

    paramsToTree code

---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var _ = require('lodash');
+
+module.exports = (params) => {
+    let toNull = value => /^null$/i.test(value) ? null : value;
+    let toNumber = value => /^\d*$/.test(value) ? +value : value;
+    let convert = _.flow(toNull, toNumber);
+
+    return _.reduce(
+        params,
+        (param, value, name) =>
+            _.set(param, name, _.isArray(value) ?
+                    value.map(convert) :
+                    convert(value)
+            )
+        , {}
+    );
+};
