de0d1e9595e54a9c586ac83f5bf36af89a477ab3
renat <gafarov.renat@gmail.com>
2017-07-21 16:12:42 +0300

    convert number in

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ var _ = require('lodash');
 
 module.exports = (params) => {
     let toNull = value => /^null$/i.test(value) ? null : value;
-    let toNumber = value => /^\d*$/.test(value) ? +value : value;
+    let toNumber = value => /^\d*$/.test(value) && +value < Number.MAX_SAFE_INTEGER ? +value : value;
     let convert = _.flow(toNull, toNumber);
 
     return _.reduce(
